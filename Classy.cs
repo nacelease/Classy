5fbd9a6391f4e559d6ec35c79753e49d551860ef
nacelease <54339008+nacelease@users.noreply.github.com>
2019-09-30 14:34:25 -0500

    Add files via upload

---
+++ Classy.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+namespace Classy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Dog = ("I am a dog");
+            Console.WriteLine("Zuko", "Husky");
+
+
+
+        }
+    }
+    class Animal
+    {
+        public string _Name { get; set; }
+        public string _Breed { get; set; }
+        public Animal(string speak)
+        {
+
+        }
+
+        public Class Dog
+
+        
+
+              (string Name, string Breed);
+
+        public string _Name { get; set; }
+        public string _Breed { get; set; }
+    
+
+
+
+    }
+    class Cat
+    {
+        public string Name;
+        public bool SixorNot;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hello, my name is" + Name + " and I am not a six toed cat");
+            Cat cat = new Cat("felix");
+
+
+
+
+
+            
+        public string name2 { get; set; }
+        public int Six { get; set; }
+    
+
+
+    } 
+    
+        
+        
+
+    }
